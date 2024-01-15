31f002f8fface58ccd9a37db9e080020e075ea9c
DuongNgoKien <manhquynh2118@gmail.com>
2024-01-15 14:18:04 +0700

    format code

--- api.py
+++ api.py
@@ -1,13 +1,12 @@
-import json
 import os
-import nltk
-import time
-from transformers import pipeline,AutoTokenizer,AutoModelForSeq2SeqLM
+
 from waitress import serve
 from flask import Flask, request, Response
-from flask_cors import CORS, cross_origin
+
 from utils import audio_extract, whisper_infer
-from inference_detect import *
+from inference_detect import sentiment_analysis_inference
+from summary import summary_infer
+
 
 app = Flask(__name__)
 
@@ -18,6 +17,7 @@ LANGUAGES = {
 }
 
 SUB_OUTPUT = "http://210.245.90.204:13000/hg_project_effect/movie_classify/subs"
+
 
 @app.route("/api/speech2text", methods=["POST", "GET"])
 def speech2text():
@@ -45,75 +45,33 @@ def speech2text():
         message = "No video path"
         return Response((message), status=400)
 
-@app.route("/api/detetcion_text", methods=["POST","GET"])
+
+@app.route("/api/summary", methods=["POST","GET"])
 def summary():
-    start_time=time.time()
-    file_path="sub_path"
-    with open(file_path,"r") as f:
-        a=f.read().strip("\n")
-    tokenizer=AutoTokenizer.from_pretrained("D:\Work\MODEL\summary-bart-large-cnn")
-    sentences=nltk.tokenize.sent_tokenize(a)
-    #Seperate the file if the file is too long compare to the model
-    length=0
-    chunk=""
-    chunks=[]
-    count=-1
-    for sentence in sentences:
-        count+=1
-        combined_length=len(tokenizer.tokenize(sentence)) +length
+    # get file sub path
+    sub_file_path = request.args.get("sub_file_path")
+    if sub_file_path is not None and os.path.exists(sub_file_path):
+        # summary file process
+        summary_path = summary_infer(sub_file_path)
         
-        if combined_length <= tokenizer.max_len_single_sentence:
-            chunk+=sentence + ""
-            length=combined_length
-            if count==len(sentences) -1:
-                chunks.append(chunk.strip())
-        else:
-            chunks.append(chunk.strip())
-            
-            length=0
-            chunk=""
-            chunk+=sentence+ " "
-            length=len(tokenizer.tokenize(sentence))
+        return {"summary_file_path": summary_path, "status": 200, "user_id": 1}
+    else:
+        message = "No file sub path"
+        return Response((message), status=400) 
 
-    inputs=[tokenizer(chunk,return_tensors="pt").to("cuda") for chunk in chunks]
-    # model=AutoModelForSeq2SeqLM.from_pretrained("pszemraj/long-t5-tglobal-base-16384-book-summary")
-    model=AutoModelForSeq2SeqLM.from_pretrained("D:\Work\MODEL\summary-bart-large-cnn").to("cuda")
-    with open("summary.txt","w") as f:
-        pass
-    for input in inputs:
-        outputs=model.generate(**input,max_length=512)
-        with open("summary.txt","a") as f:
-            f.write(tokenizer.decode(*outputs,skip_special_tokens=True))
-
-    end_time = time.time()
-    execution_time = end_time - start_time
-
-    minutes, seconds = divmod(execution_time, 60)
-    time_format = "{:02d}:{:02d}".format(int(minutes), int(seconds))
-
-    del tokenizer, model
-
-    print("Execution time:", time_format)
 
 @app.route("/api/detetcion_text", methods=["POST","GET"]) 
 def detection_text():
-    subs=pysrt.open("sub_path")
-    results = []
-    for sub in subs:
-        inputs=tokenizer(sub.text,padding=True,truncation=True,max_length=512,return_tensors="pt").to("cuda")
-        outputs=model(**inputs)
-        probs=outputs[0].softmax(1)
-        pred_label_idx=probs.argmax()
-        pred_label=model.config.id2label[pred_label_idx.item()]
-        result = {
-                'pred_label_idx': pred_label_idx.item(),
-                'pred_label': pred_label,
-                'text': sub.text,
-                'start': {'minutes': sub.start.minutes, 'seconds': sub.start.seconds},
-                'end': {'minutes': sub.end.minutes, 'seconds': sub.end.seconds}
-            }
-        results.append(result)
-    with open('results.json', 'w') as f:
-        json.dump(results, f) 
-    return results
+    # get file sub path
+    sub_file_path = request.args.get("sub_file_path")
+    if sub_file_path is not None and os.path.exists(sub_file_path):
+        # sentiment-analysis detection
+        detect_results = sentiment_analysis_inference(sub_file_path)
+        
+        return {"detect_results": detect_results, "status": 200, "user_id": 1}
+    else:
+        message = "No file sub path"
+        return Response((message), status=400)
+    
+
 serve(app, host="0.0.0.0", port=6001, threads=15)
--- inference_detect.py
+++ inference_detect.py
@@ -1,40 +1,63 @@
 import torch, os
 import pandas as pd
-from transformers import pipeline,BertForSequenceClassification, BertTokenizerFast,AutoTokenizer
 import pysrt
 import json
 
-subs = pysrt.open("D:\Work\Insidious I (2010).srt")
-# list_sub=[subs[1],subs[2],subs[3]]
+from transformers import (
+    BertForSequenceClassification,
+    AutoTokenizer
+)
 
-tokenizer=AutoTokenizer.from_pretrained("D:\Work\MODEL\Bert-horor-action")
-model=BertForSequenceClassification.from_pretrained("D:\Work\MODEL\Bert-horor-action").to("cuda")
-output=pipeline("sentiment-analysis",model=model,tokenizer=tokenizer)
 
-def inference(subs):
+def sentiment_analysis_inference(sub_file_path):
+    """Text sentiment analysis inferences.
+
+    Args:
+        sub_file_path (str or like_path): path to sub file.
+    """
+    # get sub
+    subs = pysrt.open(sub_file_path)
+    
+    # initial tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained(
+        "path_to_model"
+    )
+    model = BertForSequenceClassification.from_pretrained(
+        "path_to_model"
+    ).to("cuda")
+    
+    # get sentiment-analysis results
     results = []
     for sub in subs:
-        inputs=tokenizer(sub.text,padding=True,truncation=True,max_length=512,return_tensors="pt").to("cuda")
-        outputs=model(**inputs)
-        probs=outputs[0].softmax(1)
-        pred_label_idx=probs.argmax()
-        pred_label=model.config.id2label[pred_label_idx.item()]
+        inputs = tokenizer(
+            sub.text,
+            padding = True,
+            truncation = True,
+            max_length = 512,
+            return_tensors = "pt"
+        ).to("cuda")
+        outputs = model(**inputs)
+        probs = outputs[0].softmax(1)
+        pred_label_idx = probs.argmax()
+        pred_label = model.config.id2label[pred_label_idx.item()]
         result = {
-                'pred_label_idx': pred_label_idx.item(),
-                'pred_label': pred_label,
-                'text': sub.text,
-                'start': {'minutes': sub.start.minutes, 'seconds': sub.start.seconds},
-                'end': {'minutes': sub.end.minutes, 'seconds': sub.end.seconds}
-            }
+            'pred_label_idx': pred_label_idx.item(),
+            'pred_label': pred_label,
+            'text': sub.text,
+            'start': {
+                'minutes': sub.start.minutes,
+                'seconds': sub.start.seconds
+            },
+            'end': {'minutes': sub.end.minutes, 'seconds': sub.end.seconds}
+        }
         
-        # if pred_label=="Thriller" or pred_label=="Horror":
         results.append(result)
     
-    with open('results.json', 'w') as f:
-        json.dump(results, f)
-        
+    return results
+
+ 
 if __name__ == '__main__':
-    inference(subs)
+    pass
     
     
 
